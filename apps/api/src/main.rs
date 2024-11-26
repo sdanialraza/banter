c4be6fdc869c21781c13a1e76449c3ea9d22d1bf
Ten <32621403+TenDRILLL@users.noreply.github.com>
2024-11-26 18:06:15 +0200

    feat: ip and port loaded from env with default fallback

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -11,50 +11,57 @@ use tower_http::cors::CorsLayer;
 
 mod routes;
 
-const PORT: u16 = 3000;
-
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-
-    let address = SocketAddr::from(([127, 0, 0, 1], PORT));
+    
+    let ip = env::var("IP").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+    let address = format!("{}:{}", ip, port)
+        .parse::<SocketAddr>()
+        .expect("Invalid IP or Port");
 
     let listener = TcpListener::bind(address).await.unwrap();
 
     println!("listening on {}", listener.local_addr().unwrap());
 
-    axum::serve(listener, app()).await.unwrap();
+    axum::serve(listener, app(&ip, &port)).await.unwrap();
 }
 
-fn app() -> Router {
+fn app(ip: &str, port: &str) -> Router {
     Router::new().route("/", get(routes::index)).layer(
         CorsLayer::new()
-            .allow_origin(format!("http://localhost:{PORT}").parse::<HeaderValue>().unwrap())
+            .allow_origin(format!("http://{ip}:{port}")
+                .parse::<HeaderValue>()
+                .expect("Invalid HeaderValue"))
             .allow_methods(Method::GET),
     )
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use axum::{
         body::Body,
         http::{Request, StatusCode},
     };
+    use dotenv::dotenv;
     use http_body_util::BodyExt;
     use tower::ServiceExt;
+    use std::env;
 
     #[tokio::test]
     async fn hello_world() {
-        let app = super::app();
-
+        dotenv().ok(); 
+        
+        let ip = env::var("IP").unwrap_or_else(|_| "127.0.0.1".to_string());
+        let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
+        let app = app(&ip, &port);
         let request = Request::builder().uri("/").body(Body::empty()).unwrap();
-
         let response = app.oneshot(request).await.unwrap();
 
         assert_eq!(response.status(), StatusCode::OK);
-
         let body = response.into_body().collect().await.unwrap().to_bytes();
-
         assert_eq!(&body[..], b"Hello, world!");
     }
 }
